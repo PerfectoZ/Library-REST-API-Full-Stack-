581ea8f05fbb667d961646a721c7ba29f145215f
PerfectoZ <mandeeptaneja21@outlook.com>
2023-04-01 00:08:28 +0530

    Create Frontend Homepage and Fire up Spring Boot Backend

---
+++ frontend/react-library/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Navbar } from './Layouts/NavbarAndFooter/Navbar';
+import { Footer } from './Layouts/NavbarAndFooter/Footer';
+import { HomePage } from './Layouts/HomePage/HomePage';
+
+export const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <HomePage/>
+      <Footer/>
+    </div>
+  );
+}
