c083f5f09e2cba4b4449bb64c56a635cfd5be3dc
PerfectoZ <mandeeptaneja21@outlook.com>
2023-04-04 21:09:49 +0530

    Add Spinner

--- frontend/react-library/src/Layouts/HomePage/Components/Carousel.tsx
+++ frontend/react-library/src/Layouts/HomePage/Components/Carousel.tsx
@@ -1,6 +1,7 @@
 import { ReturnBook } from "./ReturnBook";
 import { useEffect, useState } from "react";
 import BookModel from "../../../Models/BookModel";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 
 export const Carousel = () => {
 
@@ -14,7 +15,7 @@ export const Carousel = () => {
             const url: string = `${baseUrl}?page=0&size=9`;
             const response = await fetch('http://localhost:8080/api/books?size=9&page=0');
             
-            
+
 
             if(!response.ok) {
                 throw new Error('Something went wrong!');
@@ -48,9 +49,7 @@ export const Carousel = () => {
 
     if(isLoading) {
         return(
-            <div className='container mt-5'>
-                <p> Loading... </p>
-            </div>
+            <SpinnerLoading/>
         );
     }
 
